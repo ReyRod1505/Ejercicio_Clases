ef015e3c47a03eee4c72a85b1d8113646f418c0f
Pinoso31 <jdespinozar@uamv.edu.ni>
2026-04-08 16:29:11 -0600

    Subiendo primeras cosas

--- src/Sistema_Gestion_Estudiantes/Main.java
+++ src/Sistema_Gestion_Estudiantes/Main.java
@@ -1,4 +1,41 @@
 package Sistema_Gestion_Estudiantes;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        ArrayList <Estudiante> listaEstudiante = new ArrayList<>();
+
+        int opcion;
+
+        do {
+            System.out.println("\n--- MENU ---");
+            System.out.println("1. Agregar estudiante");
+            System.out.println("2. Mostrar estudiantes");
+            System.out.println("3. Salir");
+            opcion = sc.nextInt();
+
+
+            switch(opcion) {
+                case 1:
+                    System.out.println("Ingrese el codigo del estudiante:");
+                    String codigo = sc.next();
+
+                    System.out.println("Ingrese el nombre del estudiante:");
+                    String nombre = sc.next();
+
+                    System.out.println("Ingrese el promedio del estudiante:");
+                    float promedio = sc.nextFloat();
+
+
+                    Estudiante e = new Estudiante (codigo, nombre, promedio);
+                    listaEstudiante.add(e);
+
+            }
+        } while (opcion != 3);
+        sc.close();
+
+    }
 }
